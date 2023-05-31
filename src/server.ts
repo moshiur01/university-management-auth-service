ee4d1c7138363b476fc8264aeccb4c665fe3b102
moshiur01 <mr.moshiur314@gmail.com>
2023-05-31 23:47:32 +0600

    env setup and cors setup done

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import app from "./app";
+import config from "./config";
+
+async function main() {
+  try {
+    await mongoose.connect(config.database_url as string);
+    console.log("DB Connection Done");
+    app.listen(config.port, () => {
+      console.log(`Application listening on port ${config.port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+main();
