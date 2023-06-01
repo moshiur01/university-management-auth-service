26378a0151d8ebcd83da86e7dfeabca985798cd8
moshiur01 <mr.moshiur314@gmail.com>
2023-06-01 14:11:58 +0600

    project initial setup done

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,16 @@
-import express, { Application, Request, Response } from "express";
-const app: Application = express();
-import cors from "cors";
+import express, { Application, Request, Response } from 'express'
+const app: Application = express()
+import cors from 'cors'
 
-app.use(cors());
+app.use(cors())
 
 //parser
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 //testing
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
-});
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!')
+})
 
-export default app;
+export default app
