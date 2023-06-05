c75c2a0453d520c553efab23b33b5e6697440bbd
moshiur01 <mr.moshiur314@gmail.com>
2023-06-05 06:06:55 +0600

    [DONE] create user api done

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { Application, Request, Response } from 'express'
 const app: Application = express()
 import cors from 'cors'
+import usersRouter from './app/modules/users/users.route'
 
 app.use(cors())
 
@@ -8,9 +9,12 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+// application routes
+app.use('/api/v1/users', usersRouter)
+
 //testing
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!')
+  res.send('Welcome to University Server')
 })
 
 export default app
--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,19 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./config";
+import mongoose from 'mongoose'
+
+import config from './config'
+import app from './app'
 
 async function main() {
   try {
-    await mongoose.connect(config.database_url as string);
-    console.log("DB Connection Done");
+    await mongoose.connect(config.database_url as string)
+    console.log('DB Connection Done')
+
     app.listen(config.port, () => {
-      console.log(`Application listening on port ${config.port}`);
-    });
+      console.log(`Application listening on port ${config.port}`)
+    })
   } catch (error) {
-    console.log(error);
+    console.log(error)
   }
 }
 
-main();
+main()
