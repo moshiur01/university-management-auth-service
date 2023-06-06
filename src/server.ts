5ed873f338688a200cbf8714a3415205601911c4
moshiur01 <mr.moshiur314@gmail.com>
2023-06-06 16:28:56 +0600

    [DONE] custom logger added

--- src/server.ts
+++ src/server.ts
@@ -2,17 +2,18 @@ import mongoose from 'mongoose'
 
 import config from './config'
 import app from './app'
+import { logger, ErrorLogger } from './shared/logger'
 
 async function main() {
   try {
     await mongoose.connect(config.database_url as string)
-    console.log('DB Connection Done')
+    logger.info('DB Connection Done')
 
     app.listen(config.port, () => {
-      console.log(`Application listening on port ${config.port}`)
+      logger.info(`Application listening on port ${config.port}`)
     })
   } catch (error) {
-    console.log(error)
+    ErrorLogger.error(error)
   }
 }
 
